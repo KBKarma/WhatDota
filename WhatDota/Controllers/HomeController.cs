0374066350d8a20dfa51adddef8032535564c805
KBKarma <kamikaze.bananze@gmail.com>
2019-08-31 19:27:06 +0100

    Creating method to generate random hero/role combination.

--- WhatDota/Controllers/HomeController.cs
+++ WhatDota/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web.Mvc;
+using WhatDota.Data;
 
 namespace WhatDota.Controllers
 {
@@ -13,6 +16,31 @@ namespace WhatDota.Controllers
             return View();
         }
 
-        public 
+        public string GenerateCombinations()
+        {
+            var rand = new Random();
+            var heroes = new List<Entity>();
+            var roles = new List<Entity>();
+
+            using (StreamReader r = new StreamReader("Heroes.json"))
+            {
+                string json = r.ReadToEnd();
+                heroes = JsonConvert.DeserializeObject<List<Entity>>(json);
+            }
+
+            using (StreamReader r = new StreamReader("Roles.json"))
+            {
+                string json = r.ReadToEnd();
+                roles = JsonConvert.DeserializeObject<List<Entity>>(json);
+            }
+
+            var heroId = rand.Next(0, heroes.Count);
+            var roleId = rand.Next(0, roles.Count);
+
+            var hero = heroes[heroId];
+            var role = roles[roleId];
+
+            return role.Name + " " + hero.Name;
+        }
     }
 }
